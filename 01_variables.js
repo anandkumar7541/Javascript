1402063d2cd29dd65e1714bd7413f9787ebcd2ba
anandkumar7541 <anandpatel7541@gmail.com>
2026-04-02 04:28:37 +0000

    Add basics JavaScript variables and console output

---
+++ 01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 45
+let accountEmail = "anand@gmail.com"
+var accountPassword ="4321"
+accountCity = "Patna"
+accountState = "Bihar"
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+// console.log(accountId)
