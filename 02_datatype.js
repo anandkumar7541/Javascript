5f8d90725f901cba45deecdf709560e22efeb823
anandkumar7541 <anandpatel7541@gmail.com>
2026-04-02 07:22:40 +0000

    JavaScript data types and typeof operator

---
+++ 02_datatype.js
@@ -0,0 +1,14 @@
+let name = "Anand"
+let age = 21
+let isLoggedIn = false
+let state = null
+
+
+// number => 2 to power 53
+// bigint
+// boolean => true/false
+// null => stanalone value 
+// undefined 
+
+
+console.log(typeof age)
